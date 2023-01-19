eadc4a16d20cd6ac77aa978679c6d33729bf440a
Zelin Zhu <ZZhu@esri.com>
2023-01-19 10:50:28 -0800

    Added initial files

---
+++ GISApps/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GISApps;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ GISApps/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GISApps;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
