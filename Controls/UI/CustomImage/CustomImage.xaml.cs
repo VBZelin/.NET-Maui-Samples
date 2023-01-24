4f1782d775f13b62a21b989c13c268d4363706b9
Zelin Zhu <ZZhu@esri.com>
2023-01-23 21:00:00 -0800

    Added CustomImagePage

--- Controls/UI/CustomImage/CustomImage.xaml.cs
+++ Controls/UI/CustomImage/CustomImage.xaml.cs
@@ -1,9 +1,44 @@
-﻿namespace Controls.UI;
-
-public partial class CustomImage : ContentView
+﻿namespace Controls.UI
 {
-    public CustomImage()
+    public partial class CustomImage : Frame
     {
-        InitializeComponent();
+        public static readonly BindableProperty ShowIndicatorProperty = BindableProperty.Create("ShowIndicator", typeof(bool), typeof(Frame), false, propertyChanged: OnShowIndicatorChanged);
+
+        public bool ShowIndicator
+        {
+            get => (bool)GetValue(ShowIndicatorProperty);
+            set => SetValue(ShowIndicatorProperty, value);
+        }
+
+        public static readonly BindableProperty ImageSourceProperty = BindableProperty.Create("ImageSource", typeof(ImageSource), typeof(Frame), null, propertyChanged: OnImageSourceChanged);
+
+        public ImageSource ImageSource
+        {
+            get => (ImageSource)GetValue(ImageSourceProperty);
+            set => SetValue(ImageSourceProperty, value);
+        }
+
+        public CustomImage()
+        {
+            InitializeComponent();
+
+            Indicator.IsVisible = ShowIndicator;
+            Image.Source = null;
+        }
+
+        static void OnShowIndicatorChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (CustomImage)bindable;
+
+            control.Indicator.IsVisible = (bool)newValue;
+        }
+
+        static void OnImageSourceChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (CustomImage)bindable;
+
+            control.Image.Source = null;
+            control.Image.Source = (ImageSource)newValue;
+        }
     }
 }
--- GISApps/UI/CustomImagePage.xaml.cs
+++ GISApps/UI/CustomImagePage.xaml.cs
@@ -6,5 +6,26 @@
         {
             InitializeComponent();
         }
+
+        private void OnLoadRemoteImageClicked(System.Object sender, System.EventArgs e)
+        {
+            var url = ImageUrlEntry.Text;
+
+            CustomImage.ImageSource = url;
+        }
+
+        private void OnLoadExampleOneClicked(System.Object sender, System.EventArgs e)
+        {
+            var url = "https://upload.wikimedia.org/wikipedia/commons/d/df/ArcGIS_logo.png";
+
+            CustomImage.ImageSource = url;
+        }
+
+        private void OnLoadExampleTwoClicked(System.Object sender, System.EventArgs e)
+        {
+            var url = "https://aka.ms/campus.jpg";
+
+            CustomImage.ImageSource = url;
+        }
     }
 }
